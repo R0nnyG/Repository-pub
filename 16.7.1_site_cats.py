bb0c244e81e83827e4394c9972d7de757f80f868
R0nnyG <95879115+R0nnyG@users.noreply.github.com>
2022-02-14 17:45:03 +0300

    Add files via upload

---
+++ 16.7.1_site_cats.py
@@ -0,0 +1,6 @@
+from cat_class import Cat
+cat1 = Cat("Барон", "мальчик", 2)
+cat2 = Cat("Сэм", "мальчик", 2)
+
+print("Кот:",cat1.name, cat1.gender, cat1.age)
+print("Кот:",cat2.name, cat2.gender, cat2.age)
