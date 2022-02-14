bc8e21ff4a8d514ce34a02002d78efe558f1d36b
R0nnyG <95879115+R0nnyG@users.noreply.github.com>
2022-02-14 17:48:31 +0300

    Update 16.7.1_site_cats.py

--- 16.7.1_site_cats.py
+++ 16.7.1_site_cats.py
@@ -1,6 +1,6 @@
-from cat_class import Cat
-cat1 = Cat("Барон", "мальчик", 2)
-cat2 = Cat("Сэм", "мальчик", 2)
+from cat_class import cat
+cat1 = cat("Барон", "мальчик", 2)
+cat2 = cat("Сэм", "мальчик", 2)
 
-print("Кот:",cat1.name, cat1.gender, cat1.age)
-print("Кот:",cat2.name, cat2.gender, cat2.age)
+print("Кот:", cat1.name, cat1.gender, cat1.age)
+print("Кот:", cat2.name, cat2.gender, cat2.age)
